d283b9ec3a4d74c6d4dd08ec0ff51fa9e1c2d8f5
christopher.kohlhaas@gmail.com <christopher.kohlhaas@gmail.com@f78d5ace-84b9-81af-d60f-742a6fa3a8cb>
2013-04-20 12:00:23 +0000

    disitinction between client and server plugins
    git-svn-id: https://rapla.googlecode.com/svn/plugins/holiday_plugin/trunk@3964 f78d5ace-84b9-81af-d60f-742a6fa3a8cb

--- src/org/rapla/plugin/freetime/FreetimePlugin.java
+++ src/org/rapla/plugin/freetime/FreetimePlugin.java
@@ -1,6 +1,7 @@
 package org.rapla.plugin.freetime;
 
 import org.rapla.client.ClientService;
+import org.rapla.client.ClientServiceContainer;
 import org.rapla.components.calendar.DateRenderer;
 import org.rapla.components.xmlbundle.I18nBundle;
 import org.rapla.components.xmlbundle.impl.I18nBundleImpl;
@@ -8,33 +9,22 @@ import org.rapla.framework.Configuration;
 import org.rapla.framework.Container;
 import org.rapla.framework.PluginDescriptor;
 import org.rapla.framework.TypedComponentRole;
-import org.rapla.plugin.RaplaExtensionPoints;
-import org.rapla.plugin.RaplaPluginMetaInfo;
+import org.rapla.plugin.RaplaClientExtensionPoints;
 
 
-public class FreetimePlugin implements PluginDescriptor 
+public class FreetimePlugin implements PluginDescriptor<ClientServiceContainer>
 {
     static boolean ENABLE_BY_DEFAULT = false;
     public static final TypedComponentRole<I18nBundle> RESOURCE_FILE = new TypedComponentRole<I18nBundle>(FreetimePlugin.class.getPackage().getName() + ".FreetimeResources");
     
-    public void provideServices(Container container, Configuration config) 
+    public void provideServices(ClientServiceContainer container, Configuration config) 
     {
         if (!config.getAttributeAsBoolean("enabled", ENABLE_BY_DEFAULT))
             return;
 
         container.addContainerProvidedComponent(RESOURCE_FILE, I18nBundleImpl.class, I18nBundleImpl.createConfig(RESOURCE_FILE.getId()));
-        if ( container.getContext().has( ClientService.class) ){
-            container.addContainerProvidedComponent(RaplaExtensionPoints.RESERVATION_SAVE_CHECK, FreetimeReservationSaveCheck.class);
-            container.addContainerProvidedComponent( DateRenderer.class, FreetimeHighlightRenderer.class );
-        }
-    }
-        
-    public Object getPluginMetaInfos(String key) {
-        if ( RaplaPluginMetaInfo.METAINFO_PLUGIN_ENABLED_BY_DEFAULT.equals( key )) {
-            return new Boolean( ENABLE_BY_DEFAULT );
-        }
-        
-        return null;
+        container.addContainerProvidedComponent(RaplaClientExtensionPoints.RESERVATION_SAVE_CHECK, FreetimeReservationSaveCheck.class);
+        container.addContainerProvidedComponent( DateRenderer.class, FreetimeHighlightRenderer.class );
     }
     
     public String toString() {
