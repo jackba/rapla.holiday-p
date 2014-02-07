ebe84d45e568a7835711e73165d7e6c90b12f68c
Christopher Kohlhaas <christopher.kohlhaas@gmail.com>
2014-02-07 21:46:39 +0100

    test runs under 1_7 again

--- test-src/org/rapla/plugin/tests/FreetimeCalculatorTest.java
+++ test-src/org/rapla/plugin/tests/FreetimeCalculatorTest.java
@@ -6,7 +6,6 @@ import org.rapla.RaplaTestCase;
 import org.rapla.entities.domain.Allocatable;
 import org.rapla.entities.domain.Reservation;
 import org.rapla.facade.ClientFacade;
-import org.rapla.facade.QueryModule;
 import org.rapla.framework.RaplaLocale;
 
 public class FreetimeCalculatorTest extends RaplaTestCase {
@@ -17,15 +16,13 @@ public class FreetimeCalculatorTest extends RaplaTestCase {
 
 	public void testIsFreetime() throws Exception {
 		ClientFacade facade = getFacade();
-		QueryModule tempQuery = facade;
-		// this.Query
 		Allocatable freetime = facade.newResource();
 		freetime.getClassification().setValue("name", "freetime");
 		facade.store( freetime);
 		Reservation orig = (Reservation) facade.newReservation();
 		orig.getClassification().setValue("name", "freetime");
 		RaplaLocale raplaLocale = getRaplaLocale();
-		Date start = raplaLocale.toRaplaDate(2012, 12, 1);
+		Date start = raplaLocale.toDate(2012, 12, 1);
 		Date end = raplaLocale.toDate(start, raplaLocale.toTime(12, 0, 0));
 		orig.addAppointment(facade.newAppointment(start, end));
 
